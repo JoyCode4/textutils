9d5036b5a3974e88233b628c561e7a423f7674f6
JoyCode4 <joywadhonkar4@gmail.com>
2023-04-24 12:17:53 +0530

    create a dark mode for whole website i.e. in TextForm.js,Navbar.js and App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@
 import './App.css';
 import Navbar from './Navbar';
 import TextForm from './TextForm';
+import React, {useState} from 'react';
 
 function App() {
+  const [mode,setMode] = useState("light");
+  const toggleMode = ()=>{
+    if(mode==="light"){
+      setMode("dark");
+      document.body.style.backgroundColor="#0b0a36";
+    }else{
+      setMode("light");
+      document.body.style.backgroundColor="white";
+    }
+  }
   return (
       <>
-        <Navbar titleName="TextUtils" />
-        <TextForm heading="Converter of Text" />
+        <Navbar titleName="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <TextForm heading="Converter of Text" mode={mode}/>
         {/* <About/> */}
       </>
   );
--- src/TextForm.js
+++ src/TextForm.js
@@ -1,87 +1,107 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-function TextForm(props){
-    const [text, setText] = useState("");
-    const Uppercase = ()=>{
-        let newText = text.toUpperCase();
-        setText(newText);
+function TextForm(props) {
+  const [text, setText] = useState("");
+  const Uppercase = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const Lowercase = () => {
+    let newtext = text.toLowerCase();
+    setText(newtext);
+  };
+
+  const Capitalized = () => {
+    let newData = text.toLowerCase();
+    let words = newData.split(" ");
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].substring(1);
     }
 
-    const Lowercase = () =>{
-        let newtext = text.toLowerCase();
-        setText(newtext);
+    let newText = words.join(" ");
+    setText(newText);
+  };
+
+  const Encrypt = () => {
+    let newText = "";
+    for (let i = 0; i < text.length; i++) {
+      newText = newText + text.charCodeAt(i) + ".";
     }
+    setText(newText);
+  };
 
-    const Capitalized = ()=>{
-        let newData = text.toLowerCase();
-        let words = newData.split(" ");
-        for(let i=0;i<words.length;i++){
-            words[i]=words[i][0].toUpperCase()+ words[i].substring(1);
-        }
-
-        let newText = words.join(" ");
-        setText(newText);
+  const Decrypt = () => {
+    let data = text.split(".");
+    let newText = "";
+    data.pop();
+    for (let i of data) {
+      newText = newText + String.fromCharCode(parseInt(i));
     }
+    setText(newText);
+  };
 
-    const Encrypt=()=>{
-        let newText="";
-        for(let i=0;i<text.length;i++){
-            newText = newText+text.charCodeAt(i)+".";
-        }
-        setText(newText);
-    }
+  const Clear = () => {
+    let newText = "";
+    setText(newText);
+  };
 
-    const Decrypt=()=>{
-        let data=text.split(".");
-        let newText=""
-        data.pop();
-        for(let i of data){
-            newText = newText + (String.fromCharCode(parseInt(i)));
-        }
-        setText(newText);
-    }
-
-    const Clear = ()=>{
-        let newText = "";
-        setText(newText);
-    }
-
-
-    const uppercaseChange=(e)=>{
-        setText(e.target.value);
-    }
-    return (
-        <>
-            <div className="container my-3">
-                <h1>{props.heading}</h1>
-                <div className="my-3">
-                    <textarea className="form-control" value={text} id="myBox" rows="8" placeholder="Enter Text here" onChange={uppercaseChange}></textarea>
-                </div>
-                <button className="btn btn-primary mx-3" onClick={Uppercase} >Uppercase</button>
-                <button className="btn btn-primary mx-3" onClick={Lowercase} >Lowercase</button>
-                <button className="btn btn-primary mx-3" onClick={Capitalized} >Capitalized</button>
-                <button className="btn btn-primary mx-3" onClick={Clear} >Clear Text</button>
-                <button className="btn btn-primary mx-3" onClick={Encrypt} >Encrypt</button>
-                <button className="btn btn-primary mx-3" onClick={Decrypt} >Decrypt</button>
-            </div>
-            <div className="container my-3">
-                <h1>Text Summary</h1>
-                <p>Words : {text.split(" ").length} , Characters : {text.length}</p>
-                <p>{0.008 * (text.split(" ").length)} mins read</p>
-                <h2>Preview</h2>
-                <p>
-                    {text}
-                </p>
-
-            </div>
-        </>
-
-    );
+  const uppercaseChange = (e) => {
+    setText(e.target.value);
+  };
+  return (
+    <>
+      <div className="container my-3" style={{color:props.mode==="light"?"black":"white"}}>
+        <h1>{props.heading}</h1>
+        <div className="my-3">
+          <textarea
+            className="form-control"
+            value={text}
+            id="myBox"
+            rows="8"
+            placeholder="Enter Text here"
+            onChange={uppercaseChange}
+            style={{
+              backgroundColor: props.mode === "light" ? "white" : "grey",
+              color:props.mode==="light"?"black":"white"
+            }}
+          ></textarea>
+        </div>
+        <button className={`btn btn-primary mx-3`} onClick={Uppercase}>
+          Uppercase
+        </button>
+        <button className="btn btn-primary mx-3" onClick={Lowercase}>
+          Lowercase
+        </button>
+        <button className="btn btn-primary mx-3" onClick={Capitalized}>
+          Capitalized
+        </button>
+        <button className="btn btn-primary mx-3" onClick={Clear}>
+          Clear Text
+        </button>
+        <button className="btn btn-primary mx-3" onClick={Encrypt}>
+          Encrypt
+        </button>
+        <button className="btn btn-primary mx-3" onClick={Decrypt}>
+          Decrypt
+        </button>
+      </div>
+      <div className="container my-3 " style={{color:props.mode==="light"?"black":"white"}}>
+        <h1>Text Summary</h1>
+        <p>
+          Words : {text.split(" ").length} , Characters : {text.length}
+        </p>
+        <p>{0.008 * text.split(" ").length} mins read</p>
+        <h2>Preview</h2>
+        <p>{text.length>0?text:"Enter the Text in Textbox for Preview"}</p>
+      </div>
+    </>
+  );
 }
 
 TextForm.propTypes = {
-    heading: PropTypes.string.isRequired
-}
+  heading: PropTypes.string.isRequired,
+};
 
 export default TextForm;
