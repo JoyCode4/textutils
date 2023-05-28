f1e4d62ce8c3e32e8e6b8cc3a3149803e4fae1ae
JoyCode4 <joywadhonkar4@gmail.com>
2023-05-28 16:07:00 +0530

    Revert "deployed the application on the github pages but can not use react-router-dom because it not work proper with github pages so not added the about page"
    This reverts commit 223742dd1982451d2d4e7c0b701208c1faaec28c.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import TextForm from './components/TextForm';
 import Alert from "./components/Alert";
 import About from './components/About';
 import React, {useState} from 'react';
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   const [mode,setMode] = useState("light");
@@ -37,16 +37,15 @@ const showAlert=(message,type)=>{
   }
   return (
       <>
-      {/* <Router> */}
+      <Router>
 
         <Navbar titleName="TextUtils" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
-        <TextForm heading="Converter of Text" mode={mode} showAlert={showAlert}/>
-        {/* <Routes> */}
-          {/* <Route path='/' element={<TextForm heading="Converter of Text" mode={mode} showAlert={showAlert}/>}/> */}
-          {/* <Route path='/about' element={<About />}/> */}
-        {/* </Routes> */}
-      {/* </Router> */}
+        <Routes>
+          <Route path='/' element={<TextForm heading="Converter of Text" mode={mode} showAlert={showAlert}/>}/>
+          <Route path='/about' element={<About />}/>
+        </Routes>
+      </Router>
       </>
   );
 }
